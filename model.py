fdcfce40036ee1d4b728f91bd0fbfa93bf3ba2a2
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2012-09-26 20:51:06 -0300

    Atualizado retorno do json para o cardápio do RU

--- model.py
+++ model.py
@@ -1,6 +1,13 @@
 import urllib
 from bs4 import BeautifulSoup
 import re
+from datetime import datetime
+from unicodedata import normalize
+
+
+def clean_text(text):
+    return normalize('NFKD', text.decode('utf-8')).encode('ASCII', 'ignore')
+
 
 class RU(object):
 
@@ -10,10 +17,22 @@ class RU(object):
         ru_trs = soup.find_all('tr', 'ru_head_title')
         json_as_python = {}
         for tr in ru_trs:
-            day = tr.text.strip().encode('latin-1')
+            date = tr.text.strip().encode('latin-1')
+            date = clean_text(date)
+            date_match = re.match(r'(?P<weekday>\w+) - \((?P<day>\d{2}/\d{2})\)', date)
+            date = date_match.groupdict()
+            date['weekday'] = date['weekday'].lower()
+            year = str(datetime.now().year)
+            day, month = date['day'].split('/')
+            date['day'] = '%s-%s-%s' % (year, month, day)
             content = tr.find_next('tr').find('td').text.strip()
             content = re.sub(r'[Ss]ob[\.:]\s?', 'Sobremesa: ', content)
             content = content.encode('latin-1')
             content = content.split('\n')[:-1]
-            json_as_python[day] = content
+            json_as_python[date['weekday']] = {'menu': content, 'date': date['day']}
         return json_as_python
+
+
+if __name__ == '__main__':
+    ru = RU()
+    print ru.get_menu()
